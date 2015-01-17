78bbe69017c73de613848dcce16b23ecd48f0cf4
2DanyMany5 <daniel.e.roberts@btinternet.com>
2015-01-17 14:50:45 +0000

    More Stuff

---
+++ src/main/java/com/danymany/MinerUtils/block/blockDiamondGlass.java
@@ -0,0 +1,11 @@
+package com.danymany.MinerUtils.block;
+
+public class blockDiamondGlass extends blockMU
+{
+    public blockDiamondGlass()
+    {
+        super();
+        this.setBlockName("Diamond Glass");
+        this.setBlockTextureName("blockDiamondGlass");
+    }
+}
